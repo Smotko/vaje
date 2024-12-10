65c864cdd068ee06640cdeea2e95d88045068a50
Smotko <154825828+Smotko@users.noreply.github.com>
2024-12-10 18:48:21 +0100

    Add files via upload

---
+++ assistant/Inverz.java
@@ -0,0 +1,31 @@
+/**
+ * Razred [Inverz], ki predstavlja inverz zaporedja na intervalu
+ */
+ 
+public class Inverz extends Zaporedje {
+	
+	private Zaporedje zaporedje; 	// x^2
+	private Interval interval; 		// [0, 30]
+
+	public Inverz(Zaporedje prvo, Interval interval) {
+		this.zaporedje = prvo;
+		this.interval = interval;
+	}
+
+	/** y = f(x) */
+	
+	public Integer y(int x) {
+		int zacetek = interval.vrniZacetek();
+		int konec = interval.vrniKonec();
+
+		for (int i = zacetek; i <= konec; i++) {
+			Integer y = zaporedje.y(i); // i^2
+	
+			// x --> 25 (x^2)   --->   i * i --> y == x
+			if (y != null && y == x)
+				return i;
+		}
+		return null;
+	}
+	
+}
